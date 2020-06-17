d57f25eca533f94555092054ff1ea91405002532
ytk3473 <shinta0301@gmail.com>
2020-06-17 17:53:31 +0900

    chat

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
   validates :name, presence: true, uniqueness: true
 
   has_many :group_users
+  has_many :messages
   has_many :groups, through: :group_users
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   
   root "groups#index"
   resources :users, only: [:edit, :update]
-  resources :groups, only: [:index, :create, :new, :edit, :update]
+  resources :groups, only: [:index, :create, :new, :edit, :update] do
+    resources :messages, only: [:index, :create]
+  end
 end
